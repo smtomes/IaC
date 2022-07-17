cd941fc1bcfbabe587c09214a38cabe750375b0d
smtomes <smtomes@gmail.com>
2022-07-17 13:38:33 -0500

    update

---
+++ Azure PowerShell/mountmydisk.ps1
@@ -0,0 +1,18 @@
+﻿$Disks = Get-Disk | Where partitionstyle -eq 'raw' | Sort number
+$Letters = 70..89 | ForEach-Object { [char]$_ }
+$Count = 0
+$Labels = "data1","data2"
+
+ForEach ( $disk in $disks ) 
+
+{
+
+    $driveLetter = $letters[$count].ToString()
+    
+    $disk |
+    
+    Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -UseMaximumSize -DriveLEtter $driveLetter | Format-Volume -FileSystem NTFS -NewFileSystemLabel $labels[$count] -Confirm:$false -Force
+    
+    $count++
+        
+}
